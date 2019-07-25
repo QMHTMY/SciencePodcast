6dea9d1d8119d7ce0209491fcea86bc73ecf660d
Shieber <QMH_XB_FLTMY@yahoo.com>
2019-07-25 12:25:45 +0800

    add copyrith

--- ScienceSpider.py
+++ ScienceSpider.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 #
-# 30.Apr 2019 
-# Shieber in UESTC 
-# QMH_XB_FLTMY@yahoo.com
+#    Author: Shieber
+#    Date: 2019.04.30
 #
+#                             APACHE LICENSE
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+#                            Function Description
+#    download podcasts from Science.com website.   
+#
+#    Copyright 2019 
+#    All Rights Reserved!
 
 import re
 import os
@@ -27,71 +41,72 @@ class Spider():
         self.podprefix  = 'Science-'                                  #音频前缀
         self.showstr    = 'downloading from Science... '              #下载进度显示字符
         self.pt_bs_url  = "https://www.sciencemag.org"                #podcast_base_url
-        self.pg_bs_url  = "https://www.sciencemag.org/podcasts?page=" #page_base_url
+        self.pg_bs_url  = "https://www.sciencemag.org/podcasts?page=0"#page_base_url
         self.max_job    = max_job                                     #下载进程最大数量
         self.storedir   = storedir                                    #设定存储位置名称
-        self.max_page   = self._get_max_page()
-        self.page_urls  = self._get_page_urls() 
+        self.page_urls  = self.get_page_urls()
 
         if not path.exists(self.storedir):
             os.makedirs(self.storedir)
 
+    def get_page_urls(self):
+        '''
+            提取器A: 提取最大页数，构造所有
+            页面的url列表page_urls并返回
+        '''
+        page_urls = []
 
-    #*********************1.准备文件保存目录和每一页的链接********************
-    def _get_max_page(self):
-        max_page = None
-        url = self.pg_bs_url + '0'
-        soup = self._get_url_content(url)
-        if soup:
-            last_patn = 'pager-last ellipsis last'  #硬编码，不好
-            rest = soup.find('li',class_=last_patn) 
-            max_page = int(rest.getText()[:2])      #最大页符号中类似28>>，
-        return max_page 
+        soup = self._get_url_content(self.pg_bs_url)
+        if not soup:
+            return page_urls 
 
-    def _get_page_urls(self):
-        if self.max_page == None:
-            return []
-        else:
-            return [self.pg_bs_url + str(pg_nm) for pg_nm in range(self.max_page)]
+        page_patn = 'pager-last ellipsis last'        #硬编码，不好
+        match_res = soup.find('li', class_=page_patn) 
+        max_page  = int(match_res.getText()[:2])      #最大页符号中类似28>>，
+        for page_num in range(max_page):
+            page_url = self.pg_bs_url.replace('0',str(page_num))
+            page_urls.append(page_url)
+
+        return page_urls
 
 
     #*********************2.提取播客/pdf的链接及其保存名字********************
     def _get_podcast_info(self,url):
-        radio_url  = None
-        pdf_url    = None 
-        radio_name = None
-        pdf_name   = None 
+        info_dic   = {'radio':[],'pdf':[]}
 
         soup = self._get_url_content(url)
         if soup:
             radio_url, pdf_url   = self._get_podcast_url(soup)
             radio_name, pdf_name = self._get_podcast_name(soup) 
 
-            info_dic = {'radio':[radio_url,radio_name],'pdf':[pdf_url,pdf_name]}
+            info_dic['radio'] = [radio_url, radio_name]
+            info_dic['pdf']   = [pdf_url, pdf_name]
         else:
-            info_dic = {'radio':[radio_url,radio_name],'pdf':[pdf_url,pdf_name]}
+            info_dic['radio'] = [None, None]
+            info_dic['pdf']   = [None, None]
 
         return info_dic 
 
     def _get_podcast_url(self,soup):
         patn = re.compile(r'http://(.*)\.mp3')
         link = soup.find('a',href=patn)
-        if link == None:
-            radio_url  = None
-        else:
+        if link:
             radio_url  = link['href'] 
+        else:
+            radio_url  = None
 
         patn = re.compile(r'https://(.*)\.pdf')
         link = soup.find('a',href=patn)
-        if link == None:
-            pdf_url  = None
-        else:
+        if link:
             pdf_url  = link['href'] 
+        else:
+            pdf_url  = None
 
         return radio_url, pdf_url
 
     def _get_podcast_name(self,soup):
         timestr = soup.find('time').getText()
+
         timestr = timestr.replace(',','').replace('.','') 
         timestr = timestr.split()[:3]
         middle  = ''.join([self.podprefix,'-'.join(timestr)])
@@ -121,7 +136,6 @@ class Spider():
                         stream.write(ck)
 
 
-
     #*********************4.分布式下载器**************************************
     def _download_multi(self,urls):
         '''分布式爬虫'''
@@ -129,7 +143,7 @@ class Spider():
             return 
 
         pool = Pool(self.max_job)
-        for url in set(urls):      #url去重
+        for url in urls: 
             pool.apply_async(self._func,(url,))
 
         pool.close()
@@ -139,26 +153,20 @@ class Spider():
         info_dic = self._get_podcast_info(url)
         radio_info = info_dic['radio']
         pdf_info   = info_dic['pdf']
-
         self._download_file(radio_info[0],radio_info[1]) #core function
         self._download_file(pdf_info[0],pdf_info[1])
-
-        #self.downloaded += 1
 
 
     #*********************5.提取页面播客链接**********************************
     def _get_podcast_urls(self,url):
         #linksf = soup.find_all('h2',class_="media__headline")
-
         soup = self._get_url_content(url)
         if soup:
             patn = re.compile(r'/podcast/(.*)?')
             links= soup.find_all('a',href=patn)
-
             pdcst_urls = [''.join([self.pt_bs_url + lk['href']]) for lk in links]
-            return pdcst_urls 
-        else:
-            return []
+            return set(pdcst_urls)
+        return []
 
 
     #*********************6.下载启动器****************************************
@@ -169,14 +177,12 @@ class Spider():
             html_res.encoding='utf-8'
             soup = Soup(html_res.text,'html.parser') 
             return soup
-        else:
-            return None
+        return None
 
     def control(self):
         for page_url in self.page_urls:
             podcast_urls = self._get_podcast_urls(page_url)
-            self._download_multi(podcast_urls[:4])
-            return None 
+            self._download_multi(podcast_urls)
             time.sleep(5)
 
 if __name__ == "__main__":
